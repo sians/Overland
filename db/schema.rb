f22c0736fd6a245dedcf0e0157a2db5bbcdc39a2
Sian Stone <sian.m.stone@gmail.com>
2019-09-04 11:51:29 +0100

    added json storage to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_143946) do
+ActiveRecord::Schema.define(version: 2019_09_04_104757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_09_03_143946) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.string "username"
+    t.json "storage"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
