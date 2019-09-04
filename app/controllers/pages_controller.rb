2a822dfbb8e59a2e9af6568f693d0385722f0d3d
Sian Stone <sian.m.stone@gmail.com>
2019-09-04 11:41:40 +0100

    added search form for homepage & routed to the journey_results path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def journey_results
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'test', to: 'pages#test', as:'test'
+  post 'journey_results', to: 'pages#journey_results', as: 'journey_results'
+
   resources :journeys do
     resources :connections
   end
