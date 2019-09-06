79c55a4c13ae3f3fe9a439c71893b91264f3f948
Vincent Plassard <plassard.vincent@gmail.com>
2019-09-06 14:00:10 +0100

    awesome map, markers and connections lines

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,30 @@ import { initAutocomplete } from "../plugins/init_autocomplete.js";
 import "../plugins/flatpickr";
 
 initAutocomplete();
+
+import GMaps from 'gmaps/gmaps.js';
+
+const mapElement = document.getElementById('map');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+  var flightPlanCoordinates = markers
+        var flightPath = map.drawPolyline({
+          path: flightPlanCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+     flightPath.setMap(map);
+}
