fab6a3c975c1c9681d71773ee97534c242495fb5
Vincent Plassard <plassard.vincent@gmail.com>
2019-09-09 09:26:53 +0100

    dunno

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,22 +18,18 @@ class PagesController < ApplicationController
   end
 
   def journey_results
-
-
     # start_at_date = params[:starts_at].empty? ? Date.today : Date.parse(params[:starts_at])
 
     if params[:start_city].empty? || params[:end_city].empty?
-      #raise
       redirect_to root_path, alert: "Want to stay at home? Provide a city!"
     else
       @directions = GoogleDirectionsService.new
       current_user.storage = @directions.fetch_google_directions(params[:start_city], params[:end_city])
       current_user.save
-      #geocode_cities(params[:start_city], params[:end_city])
+      geocode_cities(params[:start_city], params[:end_city])
       @route_connections = @directions.get_route_connections(params[:start_city], params[:end_city], current_user.storage)
       geocode_stopovers
     end
-    # byebug
 
   end
 
